a1e866472bf5b3877ac19a44e2afaf9885aa4e23
peternyabuto <105237692+peternyabuto@users.noreply.github.com>
2022-06-15 13:47:05 -0700

    task 9 and 11 fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - prints an integer
+ * @n: the integer to be printed
  */
 void print_number(int n)
 {
-unsigned int m, d, count;
+	unsigned int number = n;
 
-if (n < 0)
-{
-_putchar(45);
-m = n * -1;
-}
-else
-{
-m = n;
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
 
-d = m;
-count = 1;
+	if ((number / 10) > 0)
+		print_number(number / 10);
 
-while (d > 9)
-{
-d /= 10;
-count *= 10;
-}
-
-for (; count >= 1; count /= 10)
-{
-_putchar(((m / count) % 10) + 48);
-}
+	_putchar((number % 10) + '0');
 }
