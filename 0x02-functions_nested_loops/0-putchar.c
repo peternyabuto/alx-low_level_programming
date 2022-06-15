b8147bb865ed8d9e19faf2c755b6e7a51b0f96c2
peternyabuto <105237692+peternyabuto@users.noreply.github.com>
2022-06-15 15:53:47 -0700

    functions and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return 0 Always (Success)
- *
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
-int main(void) {
+int main(void)
+{
+	char abe[] = "_putchar";
 
-	_putchar("_putchar\n")
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
 	return (0);
 }
