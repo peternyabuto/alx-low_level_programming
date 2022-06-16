f202d1f560e12b611a5e7f8e9d1ad122237b5e42
peternyabuto <105237692+peternyabuto@users.noreply.github.com>
2022-06-15 17:15:38 -0700

    fixes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,4 +12,7 @@ extern void print_diagonal(int n);
 extern void print_square(int size);
 extern void print_triangle(int size);
 extern void print_number(int n);
+extern void more_numbers(void);
+extern void print_diagonal(int n);
+extern void print_triangle(int size);
 #endif
